2551aa08b58edfab167887632247501122a20403
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-02-26 17:02:24 -0700

    GRAD2-3335 - Add Metadata Endpoint to All GRAD APIs

--- api/src/main/java/ca/bc/gov/educ/api/program/config/WebSecurityConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/api/program/config/WebSecurityConfiguration.java
@@ -29,7 +29,8 @@ public class WebSecurityConfiguration {
                                 "/api/v1/api-docs/**",
                                 "/actuator/health",
                                 "/actuator/prometheus",
-                                "/health")
+                                "/health",
+                                "/api/v1/metadata")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
